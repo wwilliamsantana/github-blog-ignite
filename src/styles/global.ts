7b6830d9551ff3e894779f0f77a8b0f4d887046f
William Silva <euwilliamsantana@gmail.com>
2022-11-16 11:03:40 -0300

    Início - Configs: React-Router-dom, Styled, Themes

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus{
+    outline: 0;
+  }
+
+  body{
+    background-color: ${porps => porps.theme["base-background"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button{
+    font: 400 1rem Nunito, sans-serif;
+  }
+
+ 
+`
